675edebd8dcb4824395f0668c6e360a76fb83f1f
deepanshu7007 <deepanshu7007@gmail.com>
2023-08-14 19:35:46 +0530

    2 files added

---
+++ index.js
@@ -0,0 +1,51 @@
+var val1 = '';
+var val2 = [1, 2, 3, 4, 5];
+// console.log(typeof val1);
+// console.log(typeof null);
+// console.log(typeof undefined)
+// console.log(val2.toString())
+const val3 = parseInt(val1);
+// console.log(val3);
+// console.log(typeof isNaN)
+// console.log();
+console.log(typeof []);
+console.log(typeof '');
+console.log([1, 2] == Array('1,2'));
+console.log(Array('1,2'));
+// console.log(12 == )
+console.log(typeof typeof 'lol');
+console.log(typeof parseFloat(0.1));
+console.log(0.1);
+console.log(typeof 0.00011001100110011);
+let value1, value2;
+console.log((value1 = Number(12.5)));
+console.log((value1 = Object(NaN)));
+console.log((value2 = Object([1, 2, 3, 4, 5])));
+// value2.forEach((Element)=>{
+//   console.log(Element);
+// })
+let value3 = 'Deepanshu';
+for (let x of value2) {
+  console.log(x);
+}
+let obj = {};
+obj.rows = [1, 2, 3, 4, 5];
+obj.cols = [2, 4, 6, 8, 10];
+console.table(obj);
+console.log(obj);
+console.log(console);
+
+let vehicle = {
+  scooter: {},
+  car: {},
+  truck: {},
+};
+// vehicle.scooter.name = "Sakharam";
+// vehicle.scooter.tyres = 2;
+// vehicle.scooter.name.prop()
+
+let elmnt = document.querySelector('*');
+
+var array = [];
+array;
+console.log('Hello world');
